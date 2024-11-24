3ff5361895edac14ecec14a8098e11cfd46d1745
yf41 <yf41@st-andrews.ac.uk>
2024-11-24 04:05:47 +0000

    Switch to app router by removing pages directory

--- pages/about.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-    return (
-        <div>
-            <h1>About Me</h1>
-            <p>Hi, I'm Yukai, a programmer passionate about blogging and coding!</p>
-        </div>
-    );
-}
